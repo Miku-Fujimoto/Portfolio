98058bf8d60ad84fe7a826a250129d8048fdfd9f
Miku-Fujimoto <cocofew0411@yahoo.co.jp>
2021-02-19 02:17:41 +0900

    Added Skills

--- sketch.js
+++ sketch.js
@@ -2,21 +2,11 @@ var itr = 0;
 var scaler = 0;
 
 function setup(){
-	createCanvas(800, 500);
-	background(0);
+	createCanvas(windowWidth, windowHeight);
+	background(100);
 }
 
 function draw(){
 	translate(width/2, height/2);
 	background(0);
-	// FileList(0);
-	// drawFermatSpiral(17.0/55);
-	// itr++;
 }
-
-// function drawFermatSpiral(float rotate){
-// 	var theta = 2 * PI * itr* rotate;
-// 	PVector v = PVector.fromAngle(theta);
-// 	v.mult(scaler * SVGPathSegCurvetoQuadraticAbs(itr));
-// 	ellipse(v.x, v.y, scaler, scaler);
-// }
