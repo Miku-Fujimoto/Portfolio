91ecf339876405399e246290f73e67e920d52102
Miku-Fujimoto <cocofew0411@yahoo.co.jp>
2021-02-23 21:25:51 +0900

    p5.jsが動くように！

--- js/sketch.js
+++ js/sketch.js
@@ -1,25 +1,34 @@
-var itr = 0;
-var scaler = 0;
-
 var canvas;
 
-function setup(){
-	canvas = createCanvas(800, 500);
+let noiseScale=0.02;
+
+function windowResized(){
+    resizeCanvas(windowWidth, windowHeight);
+	background(0);
+}
+
+function setup() {
+	canvas = createCanvas(windowWidth, windowHeight, WEBGL);
 	canvas.style('z-index', '-1'); // canvasを後ろに移動する
 	background(0);
 }
 
-function draw(){
-	translate(width/2, height/2);
-	background(0);
-	// FileList(0);
-	// drawFermatSpiral(17.0/55);
-	// itr++;
-}
+function draw() {
+	for (let j = 0; j<5; j++){
+		for (let i =0; i < 5; i++){
+			let n = noise(noiseScale) * width;
+	//	translate(width/2,height/2);
+			let x =  width/20  + i * width / 20; 
+			let y = height/20  + j*height / 80;
+			let r = 2;
+		
 
-// function drawFermatSpiral(float rotate){
-// 	var theta = 2 * PI * itr* rotate;
-// 	PVector v = PVector.fromAngle(theta);
-// 	v.mult(scaler * SVGPathSegCurvetoQuadraticAbs(itr));
-// 	ellipse(v.x, v.y, scaler, scaler);
-// }
+		//	rotateX(frameCount/500);
+			rotateY(frameCount /100);
+			ellipse(x, y, r,r);
+			noStroke();
+		fill(random(255), (15-i) *255/15, (15-i) *255/15);
+		}
+	}
+
+}
